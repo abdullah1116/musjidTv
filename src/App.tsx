9433f7a9c62fb68fec4730b4f2c7ddd2bde95553
Abdullah Hasan <ahasan1116@gmail.com>
2025-05-13 18:06:32 +0500

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,197 @@
-import './App.css';
+import moment from 'moment';
+// @ts-ignore
+import iMoment from 'moment-hijri';
+import { createSignal } from 'solid-js';
+import './App.scss';
 
 function App() {
-  return <></>;
+  const [data, setData] = createSignal<AnnouncementData>(
+    {} as AnnouncementData
+  );
+  const [time, setTime] = createSignal('');
+  const [date, setDate] = createSignal('');
+  const [slideNumber, setSlideNumber] = createSignal(0);
+  const [islamicDate, setIslamicDate] = createSignal<{
+    month: string;
+    date: string;
+    year: string;
+  }>();
+
+  setInterval(() => {
+    const date = new Date();
+    const formattedTime = date.toLocaleTimeString();
+    setTime(formattedTime);
+    setDate(moment().format('MMM DD'));
+    ((window as any)['moment'] = moment)();
+    (window as any)['iMoment'] = iMoment;
+
+    const m = iMoment();
+    setIslamicDate({
+      month: m._locale._iMonths[m.iMonth()],
+      date: m.iDate(),
+      year: m.iYear(),
+    });
+  }, 1000);
+
+  setInterval(() => {
+    setSlideNumber((n) => (n + 1) % data()?.announcements?.length || 0);
+  }, 3000);
+
+  fetch('https://staging2.masjidnoormesa.com/api/announcements.php')
+    .then((res) => res.json())
+    .then((res) => setData(res));
+
+  return (
+    <>
+      <div class='background'></div>
+      <div class='layout'>
+        <div class='box time-wrap wrap'>
+          <h1 class='time'>{time()}</h1>
+          <h2 class='date'>{date()}</h2>
+          {islamicDate() && (
+            <h2 class='date-islamic'>
+              <div class='date-islamic-month'>{islamicDate()?.month}</div>
+              <div class='date-islamic-date'>{islamicDate()?.date},</div>
+              <div class='date-islamic-year'>{islamicDate()?.year}</div>
+            </h2>
+          )}
+        </div>
+
+        <div class='musjid-info-wrap wrap warp-light'>
+          <div class='box'>
+            <span class='box-heading'>Musjid Al-Noor</span>
+            <div class='box-content'>
+              <div>
+                <div>602-816-7428</div>
+                <div>imam@masjidnoormesa.com</div>
+                <div>55 N Matlock, Mesa, AZ 85203</div>
+              </div>
+              <div class='qr-code'>
+                <img src='/assets/qr-code.svg' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {data()?.announcements?.length && (
+          <div class='slide-info-wrap wrap warp-light'>
+            <div class='box'>
+              <div class='box-heading'>
+                <div>
+                  {' '}
+                  {data().announcements[slideNumber()]?.btn_label ||
+                    `Slide ${slideNumber()}`}
+                </div>
+                <div>
+                  {data().announcements.length > 1 &&
+                    data().announcements.map((_, i) => (
+                      <>
+                        <button class='btn' onclick={() => setSlideNumber(i)}>
+                          {i === slideNumber() ? 'O' : '-'}
+                        </button>
+                      </>
+                    ))}
+                </div>
+              </div>
+              <div
+                class='box-content'
+                innerHTML={
+                  data().announcements[slideNumber()]?.announcement || ''
+                }
+              ></div>
+            </div>
+          </div>
+        )}
+
+        <div class='up-coming-wrap wrap'>
+          <div class='box'>
+            <span class='box-heading'>Next Iqmah</span>
+            <div class='box-content '>
+              <span class='up-coming'>5:30s</span>
+            </div>
+          </div>
+        </div>
+        <div class='timetable-wrap wrap'>
+          <div class='box'>
+            <span class='box-heading'>Prayer Timetable</span>
+            {data()?.currentTime?.map((time) => (
+              <div class='table-row'>
+                <div class='name'>{time.prayername}</div>
+                <div class='begins'>
+                  {
+                    ((data().startTime as any)[
+                      String.fromCharCode(97 + time.timeid)
+                    ] || {}) as any
+                  }
+                </div>
+                <div class='starts'>
+                  <div class='starts-time'>
+                    {time.timehour}:{time.timeminute} {time.timeampm}
+                  </div>
+                  <div class='change'>6:10 after 2 days</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
+
+export interface CurrentTimeItem {
+  timeid: number;
+  dateC: string;
+  prayername: string;
+  timehour: number;
+  timeminute: string;
+  timeampm: string;
+  time: string;
+}
+
+export interface StartTime {
+  a: string;
+  b: string;
+  c: string;
+  d: string;
+  e: string;
+  f: string;
+}
+
+export interface AnnouncementItem {
+  id: number;
+  announcement: string;
+  btn_label: string;
+  btn_url: string;
+  position: number;
+}
+
+export interface RecordingItem {
+  id: number;
+  Speaker: string;
+  Category: string;
+  Topic: string;
+  URL: string;
+  Recording_Date: string;
+}
+
+export interface Pages {
+  page: number;
+  totalPages: number;
+}
+
+export interface InfoItem {
+  id: string;
+  data: string;
+}
+
+export interface AnnouncementData {
+  currentTime: CurrentTimeItem[];
+  startTime: StartTime;
+  announcements: AnnouncementItem[];
+  recordings: RecordingItem[];
+  pages: Pages;
+  info: InfoItem[];
+}
---
+++ src/testData.ts
@@ -0,0 +1,176 @@
+export const announcement = {
+  currentTime: [
+    {
+      timeid: 1,
+      dateC: '2025-05-09',
+      prayername: 'Fajar',
+      timehour: 4,
+      timeminute: '45',
+      timeampm: 'AM',
+      time: '04:45',
+    },
+    {
+      timeid: 2,
+      dateC: '2021-10-15',
+      prayername: 'Zuhar',
+      timehour: 1,
+      timeminute: '15',
+      timeampm: 'PM',
+      time: '13:15',
+    },
+    {
+      timeid: 3,
+      dateC: '2025-04-10',
+      prayername: 'Asr',
+      timehour: 5,
+      timeminute: '30',
+      timeampm: 'PM',
+      time: '17:30',
+    },
+    {
+      timeid: 4,
+      dateC: '2021-10-15',
+      prayername: 'Maghrib',
+      timehour: 6,
+      timeminute: '00',
+      timeampm: 'PM',
+      time: '18:00',
+    },
+    {
+      timeid: 5,
+      dateC: '2025-04-26',
+      prayername: 'Isha',
+      timehour: 9,
+      timeminute: '00',
+      timeampm: 'PM',
+      time: '21:00',
+    },
+    {
+      timeid: 6,
+      dateC: '2021-10-15',
+      prayername: 'Jumma',
+      timehour: 1,
+      timeminute: '30',
+      timeampm: 'PM',
+      time: '13:30',
+    },
+  ],
+  startTime: {
+    a: '3:55',
+    b: '12:29',
+    c: '5:16',
+    d: '7:21',
+    e: '8:53',
+    f: '12:29',
+  },
+  announcements: [
+    {
+      id: 35,
+      announcement:
+        '<p>Please see Maktab Al-Noor student enrollment form for new student registration.</p>',
+      btn_label: 'Enrollment Form',
+      btn_url: 'https://masjidnoormesa.com/form.pdf',
+      position: 0,
+    },
+  ],
+  recordings: [
+    {
+      id: 44,
+      Speaker: 'Shaikh Kashif Mansoor',
+      Category: 'Durs Quran',
+      Topic: 'Surah Mulk #30',
+      URL: 'https://www.dropbox.com/s/0c3dfx507xhf19c/2021-12-15-Mulk-30.mp3?raw=1',
+      Recording_Date: '2021-12-15',
+    },
+    {
+      id: 43,
+      Speaker: 'Shaikh Kashif Mansoor',
+      Category: 'Durs Quran',
+      Topic: 'Surah Mulk #29',
+      URL: 'https://www.dropbox.com/s/a9dzd0blb9ev92r/2021-12-14-Mulk-29.mp3?raw=1',
+      Recording_Date: '2021-12-14',
+    },
+    {
+      id: 42,
+      Speaker: 'Shaikh Kashif Mansoor',
+      Category: 'Durs Quran',
+      Topic: 'Surah Mulk #26 ',
+      URL: 'https://www.dropbox.com/s/p672hfxnutotgr5/2021-12-08-Mulk-26.mp3?raw=1',
+      Recording_Date: '2021-12-08',
+    },
+    {
+      id: 41,
+      Speaker: 'Shaikh Kashif Mansoor',
+      Category: 'Durs Quran',
+      Topic: 'Surah Mulk #25',
+      URL: 'https://www.dropbox.com/s/npdy476syrhlu99/2021-12-07-Mulk-25.mp3?raw=1',
+      Recording_Date: '2021-12-07',
+    },
+    {
+      id: 40,
+      Speaker: 'Shaikh Kashif Mansoor',
+      Category: 'Durs Quran',
+      Topic: 'Surah Mulk #24',
+      URL: 'https://www.dropbox.com/s/hwd7ljd4o84ejaz/2011-12-06-Mulk24.mp3?raw=1',
+      Recording_Date: '2021-12-06',
+    },
+  ],
+  pages: {
+    page: 1,
+    totalPages: 8,
+  },
+  info: [
+    {
+      id: 'twitter',
+      data: '',
+    },
+    {
+      id: 'services',
+      data: "<p>Prayers</p><p>Qur'an reading</p><p>Qur'an Memorization</p><p>Nikah</p>",
+    },
+    {
+      id: 'phone',
+      data: '602-816-7428',
+    },
+    {
+      id: 'nav_logo',
+      data: 'nav_logo',
+    },
+    {
+      id: 'maps_iframe',
+      data: 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6660.499693509907!2d-111.807942!3d33.41673!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba62d3c68af85%3A0x99de4f488f397c5b!2sMasjid+Al-Noor!5e0!3m2!1sen!2sus!4v1561908529132!5m2!1sen!2sus',
+    },
+    {
+      id: 'insta',
+      data: '',
+    },
+    {
+      id: 'footer_logo',
+      data: 'footer_logo',
+    },
+    {
+      id: 'fb',
+      data: 'https://www.facebook.com/pages/Masjid-Al-Noor/182964518527416',
+    },
+    {
+      id: 'email',
+      data: 'imam@masjidnoormesa.com',
+    },
+    {
+      id: 'banner_img_2',
+      data: 'banner_img_2',
+    },
+    {
+      id: 'banner_img_1',
+      data: 'banner_img_1',
+    },
+    {
+      id: 'address',
+      data: '<p>55 N Matlock, Mesa, AZ 85203</p>',
+    },
+    {
+      id: 'about',
+      data: '<p>Masjid Al-Noor was founded in 1998.</p><p>It is situated at 55 N Matlock, Mesa, AZ 85203</p>',
+    },
+  ],
+};
